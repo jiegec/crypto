3f824161afe4f8026a2ded583912d6872f8b7ebc
Jiajie Chen <c@jia.je>
2020-04-28 23:59:49 +0800

    Add sboxes and initial f function

--- des.cpp
+++ des.cpp
@@ -3,7 +3,10 @@
 
 using namespace std;
 
-// taken from https://en.wikipedia.org/wiki/DES_supplementary_material
+// reference:
+// http://page.math.tu-berlin.de/~kant/teaching/hess/krypto-ws2006/des.htm
+// tables are taken
+// from https://en.wikipedia.org/wiki/DES_supplementary_material
 
 // Initial permutation
 const int ip[] = {58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4,
@@ -26,6 +29,41 @@ const int e[] = {32, 1,  2,  3,  4,  5,  4,  5,  6,  7,  8,  9,  8,  9,  10, 11,
 // Permutation
 const int p[] = {16, 7, 20, 21, 29, 12, 28, 17, 1,  15, 23, 26, 5,  18, 31, 10,
                  2,  8, 24, 14, 32, 27, 3,  9,  19, 13, 30, 6,  22, 11, 4,  25};
+
+// 8 S-boxes
+const int s[8][64] = {
+    {14, 4,  13, 1, 2,  15, 11, 8,  3,  10, 6,  12, 5,  9,  0, 7,
+     0,  15, 7,  4, 14, 2,  13, 1,  10, 6,  12, 11, 9,  5,  3, 8,
+     4,  1,  14, 8, 13, 6,  2,  11, 15, 12, 9,  7,  3,  10, 5, 0,
+     15, 12, 8,  2, 4,  9,  1,  7,  5,  11, 3,  14, 10, 0,  6, 13},
+    {15, 1,  8,  14, 6,  11, 3,  4,  9,  7, 2,  13, 12, 0, 5,  10,
+     3,  13, 4,  7,  15, 2,  8,  14, 12, 0, 1,  10, 6,  9, 11, 5,
+     0,  14, 7,  11, 10, 4,  13, 1,  5,  8, 12, 6,  9,  3, 2,  15,
+     13, 8,  10, 1,  3,  15, 4,  2,  11, 6, 7,  12, 0,  5, 14, 9},
+    {10, 0,  9,  14, 6, 3,  15, 5,  1,  13, 12, 7,  11, 4,  2,  8,
+     13, 7,  0,  9,  3, 4,  6,  10, 2,  8,  5,  14, 12, 11, 15, 1,
+     13, 6,  4,  9,  8, 15, 3,  0,  11, 1,  2,  12, 5,  10, 14, 7,
+     1,  10, 13, 0,  6, 9,  8,  7,  4,  15, 14, 3,  11, 5,  2,  12},
+    {7,  13, 14, 3, 0,  6,  9,  10, 1,  2, 8, 5,  11, 12, 4,  15,
+     13, 8,  11, 5, 6,  15, 0,  3,  4,  7, 2, 12, 1,  10, 14, 9,
+     10, 6,  9,  0, 12, 11, 7,  13, 15, 1, 3, 14, 5,  2,  8,  4,
+     3,  15, 0,  6, 10, 1,  13, 8,  9,  4, 5, 11, 12, 7,  2,  14},
+    {2,  12, 4,  1,  7,  10, 11, 6,  8,  5,  3,  15, 13, 0, 14, 9,
+     14, 11, 2,  12, 4,  7,  13, 1,  5,  0,  15, 10, 3,  9, 8,  6,
+     4,  2,  1,  11, 10, 13, 7,  8,  15, 9,  12, 5,  6,  3, 0,  14,
+     11, 8,  12, 7,  1,  14, 2,  13, 6,  15, 0,  9,  10, 4, 5,  3},
+    {12, 1,  10, 15, 9, 2,  6,  8,  0,  13, 3,  4,  14, 7,  5,  11,
+     10, 15, 4,  2,  7, 12, 9,  5,  6,  1,  13, 14, 0,  11, 3,  8,
+     9,  14, 15, 5,  2, 8,  12, 3,  7,  0,  4,  10, 1,  13, 11, 6,
+     4,  3,  2,  12, 9, 5,  15, 10, 11, 14, 1,  7,  6,  0,  8,  13},
+    {4,  11, 2,  14, 15, 0, 8,  13, 3,  12, 9, 7,  5,  10, 6, 1,
+     13, 0,  11, 7,  4,  9, 1,  10, 14, 3,  5, 12, 2,  15, 8, 6,
+     1,  4,  11, 13, 12, 3, 7,  14, 10, 15, 6, 8,  0,  5,  9, 2,
+     6,  11, 13, 8,  1,  4, 10, 7,  9,  5,  0, 15, 14, 2,  3, 12},
+    {13, 2,  8,  4, 6,  15, 11, 1,  10, 9,  3,  14, 5,  0,  12, 7,
+     1,  15, 13, 8, 10, 3,  7,  4,  12, 5,  6,  11, 0,  14, 9,  2,
+     7,  11, 4,  1, 9,  12, 14, 2,  0,  6,  10, 13, 15, 3,  5,  8,
+     2,  1,  14, 7, 4,  10, 8,  13, 15, 12, 9,  0,  3,  5,  6,  11}};
 
 // key schedule
 const int pc1[] = { // left
@@ -102,5 +140,41 @@ void des_cbc(const vector<uint8_t> &input, const vector<uint8_t> &key,
     uint64_t after_pc2 = apply_permutation<48>(current_key, pc2);
     subkeys[i] = after_pc2;
     // printf("Subkey %d: %llx\n", i, after_pc2);
+  }
+
+  // for each block
+  for (int offset = 0; offset < input.size(); offset += 64) {
+    // convert data to 64bit integer
+    uint64_t init_data = 0;
+    for (int i = 0; i < 8; i++) {
+      // reverse bit order
+      init_data |= (uint64_t)(reverse(input[offset + i])) << (8 * i);
+    }
+
+    // ip
+    uint64_t after_ip = apply_permutation<64>(init_data, ip);
+    // printf("after ip: %llx\n", after_ip);
+
+    // swap left and right before first round
+    uint64_t left = after_ip & (((uint64_t)1 << 32) - 1);
+    uint64_t right = after_ip >> 32;
+    printf("right: %llx\n", right);
+
+    for (int round = 0; round < 16; round++) {
+      // expand
+      uint64_t after_expansion = apply_permutation<48>(right, e);
+      // printf("e(right): %llx\n", after_expansion);
+      // xor with subkey
+      uint64_t xored = after_expansion ^ subkeys[round];
+      // printf("e(right)^subkey: %llx\n", xored);
+      // split xored into 8 bytes and pass to each s-box
+
+      // F-function result
+      uint64_t f;
+      uint64_t new_left = right;
+      uint64_t new_right = left ^ f;
+      left = new_left;
+      right = new_right;
+    }
   }
 }
