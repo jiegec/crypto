581d417e3e8803826e3df717c032a64d5e52af30
Jiajie Chen <c@jia.je>
2020-04-29 19:30:20 +0800

    Add test for sha512

--- test.cpp
+++ test.cpp
@@ -177,3 +177,23 @@ TEST_CASE("SHA224", "") {
     REQUIRE(vec_output == parse_hex_new(output));
   }
 }
+
+TEST_CASE("SHA512", "") {
+  std::vector<uint8_t> vec_output;
+  SECTION("sha512 of test") {
+    std::string input = "74657374";
+    std::string output =
+        "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185"
+        "f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff";
+    sha512(parse_hex_new(input), vec_output);
+    REQUIRE(vec_output == parse_hex_new(output));
+  }
+  SECTION("sha512 of empty") {
+    std::string input = "";
+    std::string output =
+        "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d1"
+        "3c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e";
+    sha512(parse_hex_new(input), vec_output);
+    REQUIRE(vec_output == parse_hex_new(output));
+  }
+}
